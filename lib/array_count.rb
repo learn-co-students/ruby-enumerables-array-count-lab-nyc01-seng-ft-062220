7bb79532bdbdd64854c08666d587e57b2417f523
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-06-01 12:10:13 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,20 @@
 def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
+  count = 0
+  array.count do |item|
+    if item.class == String
+      count += 1
+    end
+  end
+  return count
 end
 
+
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+  count = 0
+    array.count do |item|
+      if item == ""
+        count += 1
+      end
+    end
+    return count
 end
--- spec/array_count_spec.rb
+++ spec/array_count_spec.rb
@@ -1,19 +1,21 @@
-require 'spec_helper'
-
-array_1 = ["", "hello", "world", 4, 1, 5, [], {}]
-array_2 = ["", 4, "", "goodbye",""]
-
-describe 'count_strings' do
-  it 'takes in an array of varying data types and returns the total number of strings' do
-    expect(count_strings(array_1)).to eq(3)
-    expect(count_strings(array_2)).to eq(4)
+def count_strings(array)
+  count = 0
+  array.count do |item|
+    if item.class == String
+      count += 1
+    end
   end
+  return count
 end
 
 
-describe 'count_empty_strings' do
-  it 'takes in an array of varying data types and returns the total number of EMPTY strings' do
-    expect(count_empty_strings(array_1)).to eq(1)
-    expect(count_empty_strings(array_2)).to eq(3)
-  end
+def count_empty_strings(array)
+  count = 0
+    array.count do |item|
+      if item == ""
+        count += 1
+      end
+    end
+    return count
 end
+
