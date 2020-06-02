df0f01828c030ca474e288bf7ed99aaa8f012493
fearless-command-2358 <iuriseara@gmail.com>
2020-06-02 21:36:20 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,21 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  index = 0
+  array.count do |element|
+    if element.class == String
+      index += 1
+    end
+  end
+  return index
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  index = 0
+  array.count do |element|
+    if element == ""
+      index += 1
+    end
+  end
+  return index
 end
