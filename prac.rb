7365c1577d9db9b5d34146abb35c1eadcd3622b1
Alex Beciana <alex.beciana@gmail.com>
2020-06-13 16:49:08 +0000

    Done.

---
+++ prac.rb
@@ -0,0 +1,11 @@
+testing = ["hi", "hello", "what", 5, 6, 7]
+
+
+def count_strings(array)
+  
+  array.count if array[index].class == String
+  end
+  return array
+end
+
+puts count_strings(testing)
